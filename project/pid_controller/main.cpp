eecd7e6963a75bc055780839cb919355a60478bc
nieuwmijnleven <nieuwmijnleven@outlook.com>
2021-10-20 18:17:15 +0200

    modified main.cpp

--- project/pid_controller/main.cpp
+++ project/pid_controller/main.cpp
@@ -220,7 +220,7 @@ int main ()
   **/
 
   PID pid_steer = PID();
-  pid_steer.Init(0.2, 0.5, 0.004, 1.2, -1.2);
+  pid_steer.Init(0.001, 0.001, 0.001, 1.2, -1.2);
 
   // initialize pid throttle
   /**
@@ -228,7 +228,7 @@ int main ()
   **/
 
   PID pid_throttle = PID();
-  pid_throttle.Init(0.2, 0.5, 0.004, 1, -1);
+  pid_throttle.Init(0.001, 0.001, 0.001, 1, -1);
 
   h.onMessage([&pid_steer, &pid_throttle, &new_delta_time, &timer, &prev_timer, &i, &prev_timer](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, uWS::OpCode opCode)
   {
